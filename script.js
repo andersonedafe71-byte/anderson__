8195742fb43de33541e7751d782a5a32634b37d5
andersonedafe71-byte <andersonedafe71@gmail.com>
2025-12-19 02:52:41 -1100

    Update script.js

--- script.js
+++ script.js
@@ -29,29 +29,6 @@ document.addEventListener("DOMContentLoaded", () => {
         update();
     });
 });
-.gallery-img {
-  width: 100%;
-  cursor: pointer;
-}
 
-/* Modal background */
-.modal {
-  display: none;
-  position: fixed;
-  z-index: 9999;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.9);
-  align-items: center;
-  justify-content: center;
-}
-
-/* Big image */
-.modal img {
-  max-width: 95%;
-  max-height: 95%;
-}
 
 
